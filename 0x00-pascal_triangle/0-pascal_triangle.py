7c154c8695b8e021fe9d9d7210dc8a7f062fa39f
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-24 11:14:51 +0200

    Add documentation to the module and function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Module that contains a function to generate the pascal triangle
+"""
 
 def pascal_triangle(n:int) -> list:
+    """
+    returns list of lists of integers representing the Pascal’s triangle of n
+
+    Args:
+        n (int): size of the triangle
+
+    Returns:
+        list: the list of lists containing values for the pascal's triangle
+    """
     res = [[1]]
     for i in range(n - 1):
         temp = [0] + res[-1] + [0]
