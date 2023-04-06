c9a6e9145d589f2248fae5f0ad4c40f68b379a9f
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-07 01:37:00 +0200

    Create a function to calculate the minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations needed to result in
+    exactly n H characters in the file.
+
+    Args:
+        n (int): Number of characters
+
+    Returns:
+        int: Fewest number of operations needed to result in exactly n H
+        characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    i = 2
+    count = 0
+
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n /= i
+        else:
+            i += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    n = 4
+    print("Min # of operations to reach {} characters: {}"
+          .format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} characters: {}"
+          .format(n, minOperations(n)))
