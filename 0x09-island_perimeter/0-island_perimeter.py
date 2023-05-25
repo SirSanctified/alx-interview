81002b8b1fa1bb9d4f70f19e4d951ac3de6ce7e3
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-25 11:57:13 +0200

    Add the island perimeter interview question solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+
+    Args:
+        grid (List[int]): list of list of integers: 0 represents water,
+        1 represents land
+    """
+    visited = set()
+
+    def dfs(i, j):
+        """
+        Args:
+            i (int): row index
+            j (int): column index
+        """
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or \
+                grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+        visited.add((i, j))
+        perim = dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+        return perim
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                return dfs(i, j)
